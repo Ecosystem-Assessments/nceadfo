f951d72a9f9b1f607cd035cfa206bef2d94665d5
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-21 15:07:41 -0400

    restructure pipedat yaml

--- R/integrate_data.R
+++
@@ -1,9 +0,0 @@
-#' Script to integrate and prepare data for analysis using the pipedat package
-#'
-#' @export
-integrate_data <- function() {
-  uid <- param$pipedat$integration_pipelines
-  crs <- param$crs
-  grd_poly <- sf::st_read("data/data-grid/grid_poly.geojson")
-  pipedat::pipin(uid, grid = grd_poly)
-}
