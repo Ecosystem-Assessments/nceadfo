eb105848157ff6a39ba685de07b93738225fd889
David Beauchesne <david.beauchesne@uqar.ca>
2023-02-14 10:58:23 -0500

    render all figures and update atlas

--- R/fig_atlas.R
+++ R/fig_atlas.R
@@ -1,102 +1,23 @@
 #' Export figures for atlas
 #'
 #' @export
-figures <- function() {
-  out <- list()
-  out$out <- here::here("figures")
-  out$biotic_cnt <- here::here(out$out,"biotic","continuous")
-  out$biotic_bin <- here::here(out$out,"biotic","binary")
-  out$abiotic <- here::here(out$out, "abiotic")
-  out$drivers <- here::here(out$out,"drivers")
-  out$cea_species <- here::here(out$out,"cea_species")
-  out$cea_network <- here::here(out$out,"cea_network")
-  lapply(out, chk_create)
-  
-  # ---
-  plotDat <- function(dat, out, suffix = "") {
-    nm <- tools::file_path_sans_ext(names(dat))
-    png(
-      here::here(out, glue::glue("{nm}{suffix}.png")), 
-      res = param$figures$resolution, 
-      width = param$figures$width, 
-      height = param$figures$height, 
-      units = "mm", 
-      pointsize = param$figures$pointsize
-    )
-    plot_nceadfo(dat)
-    dev.off()
-  }
-  
-  # Species distribution continuous
-  dir(
-    c("data/data-biotic/marine_mammals/continuous",
-      "data/data-biotic/sea_birds/continuous", 
-      "data/data-biotic/marine_species/random_forest_regression_smoothing"), 
-    full.names = TRUE
-  ) |>
-  lapply(stars::read_stars) |>
-  lapply(plotDat, out$biotic_cnt)
-
-  # Species distribution binary
-  dir(
-    c("data/data-biotic/marine_mammals/binary",
-      "data/data-biotic/sea_birds/binary", 
-      "data/data-biotic/marine_species/random_forest_regression_binary"), 
-    full.names = TRUE
-  ) |>
-  lapply(stars::read_stars) |>
-  lapply(plotDat, out$biotic_bin)
-
-  # Abiotic  
-  dir("data/data-abiotic", full.names = TRUE, pattern = ".tif$") |>
-  lapply(stars::read_stars) |>
-  lapply(plotDat, out$abiotic)
-  
-  # Drivers 
-  dr <- here::here("data","cea_modules","drivers")
-  per <- dir(dr)
-  for(i in 1:length(per)) {
-    here::here(dr, per[i]) |>
-    dir(full.names = TRUE, pattern = ".tif$") |>
-    lapply(stars::read_stars) |>
-    lapply(plotDat, out$drivers)
-  }
-
-  # Species-scale cumulative effects assessment
-  dr <- here::here("output","cea_species")
-  per <- dir(dr)
-  outsp <- here::here(out$cea_species, per)
-  lapply(outsp, chk_create)
-  for(i in 1:length(per)) {
-    here::here(dr, per[i]) |>
-    dir(full.names = TRUE, pattern = ".tif$") |>
-    lapply(stars::read_stars) |>
-    lapply(plotDat, outsp[i])
-  }
-  
-  # # Network-scale cumulative effects assessment
-  dr <- here::here("output","cea_network")
-  per <- dir(dr)
-  outsp <- here::here(out$cea_network, per)
-  lapply(outsp, chk_create)
-  for(i in 1:length(per)) {
-    here::here(dr, per[i], "cea") |>
-    dir(full.names = TRUE, pattern = ".tif$") |>
-    lapply(stars::read_stars) |>
-    lapply(plotDat, outsp[i])
-  }
-}
-
-
-#' Export figures for atlas
-#'
-#' @export
-fig_atlas <- function(type = c("species","drivers")) {
+fig_atlas <- function(type = c("footprint","cea","species","drivers")) {
   # Parameters 
   hts <- 300
   img_resize <- "30%x30%"
-  img1_loc <- "+2675+300"
-  img2_loc <- "+5825+300"
+
+  # Text position
+  x1 <- 3250
+  x2 <- 100
+  y1 <- 300
+  yG <- 2830
+  t1_1 <- glue::glue("+{x1}+{y1}")
+  t2_1 <- glue::glue("+{x2}+{y1}")
+  t1_2 <- glue::glue("+{x1}+{y1 + yG}")
+  t2_2 <- glue::glue("+{x2}+{y1 + yG}")
+  t1_3 <- glue::glue("+{x1}+{y1 + (yG*2)}")
+  t2_3 <- glue::glue("+{x2}+{y1 + (yG*2)}")
+
   
   # Functions
   nm_title <- function(img, chr) {
@@ -130,6 +51,7 @@ fig_atlas <- function(type = c("species","drivers")) {
     magick::image_annotate(
       img,
       chr,
+      gravity = "northeast",
       location = location,
       size = 70,
       font = "Palatino",
@@ -155,19 +77,150 @@ fig_atlas <- function(type = c("species","drivers")) {
   }
 
   # ----------------------------------------------------------------------------------------
+  if ("footprint" %in% type) {
+    out <- list()
+    out$figs <- here::here("figures","footprint")
+    out$atlas <- here::here("figures","atlas","footprint")
+    chk <- file.exists(out$figs)
+    stopifnot(chk)
+    chk_create(out$atlas)
+    figs <- dir(out$figs, full.names = TRUE)
+    
+    # -----------------
+    # Species-scale cea
+    i1 <- magick::image_read(here::here(out$figs, "cumulative_drivers-2010_2015.png"))
+    i2 <- magick::image_read(here::here(out$figs, "cumulative_drivers-2016_2021.png"))
+    img <- magick::image_append(c(i1,i2))
+
+    # Add border 
+    ht <- magick::image_info(img)$height 
+    img <- magick::image_border(img, glue::glue("0x{hts}"), color = "#ffffff") |>
+           magick::image_crop(glue::glue("0x{ht+hts}")) 
+
+    # Add text       
+    img <- nm_title(img, "Cumulative drivers")
+    img <- nm_sub2(img, "2010-2015", t1_1)
+    img <- nm_sub2(img, "2016-2021", t2_1)
+
+    # Resize 
+    img <- magick::image_resize(img, img_resize)
+
+    # Export  
+    img_write(img, here::here(out$atlas, "cumulative_drivers.png"))
+    rm(img)
+    gc()
+    
+    # -----------------
+    # Species richness
+    img <- magick::image_read(here::here(out$figs, "cumulative_species.png"))
+
+    # Add border 
+    ht <- magick::image_info(img)$height 
+    img <- magick::image_border(img, glue::glue("0x{hts}"), color = "#ffffff") |>
+           magick::image_crop(glue::glue("0x{ht+hts}")) 
+
+    # Add text       
+    img <- nm_title(img, "Species richness")
+
+    # Resize 
+    img <- magick::image_resize(img, img_resize)
+
+    # Export  
+    img_write(img, here::here(out$atlas, "species_richness.png"))
+    rm(img)
+    gc()
+  }
+  
+  # ----------------------------------------------------------------------------------------
+  if ("cea" %in% type) {
+    out <- list()
+    out$figs <- here::here("figures","cea")
+    out$atlas <- here::here("figures","atlas","cea")
+    chk <- file.exists(out$figs)
+    stopifnot(chk)
+    chk_create(out$atlas)
+    figs <- dir(out$figs, full.names = TRUE)
+    
+    # -----------------
+    # Species-scale cea
+    i1 <- magick::image_read(here::here(out$figs, "cea_species-2010_2015.png"))
+    i2 <- magick::image_read(here::here(out$figs, "cea_species-2016_2021.png"))
+    img <- magick::image_append(c(i1,i2))
+
+    # Add border 
+    ht <- magick::image_info(img)$height 
+    img <- magick::image_border(img, glue::glue("0x{hts}"), color = "#ffffff") |>
+           magick::image_crop(glue::glue("0x{ht+hts}")) 
+
+    # Add text       
+    img <- nm_title(img, "Species-scale cumulative effects")
+    img <- nm_sub2(img, "2010-2015", t1_1)
+    img <- nm_sub2(img, "2016-2021", t2_1)
+
+    # Resize 
+    img <- magick::image_resize(img, img_resize)
+
+    # Export  
+    img_write(img, here::here(out$atlas, "cea_species.png"))
+    rm(img)
+    gc()
+    
+    # -----------------
+    # Network-scale cea
+    i1 <- magick::image_read(here::here(out$figs, "cea_network-2010_2015.png"))
+    i2 <- magick::image_read(here::here(out$figs, "cea_network-2016_2021.png"))
+    img <- magick::image_append(c(i1,i2))
+
+    # Add border 
+    ht <- magick::image_info(img)$height 
+    img <- magick::image_border(img, glue::glue("0x{hts}"), color = "#ffffff") |>
+           magick::image_crop(glue::glue("0x{ht+hts}")) 
+
+    # Add text       
+    img <- nm_title(img, "Network-scale cumulative effects")
+    img <- nm_sub2(img, "2010-2015", t1_1)
+    img <- nm_sub2(img, "2016-2021", t2_1)
+
+    # Resize 
+    img <- magick::image_resize(img, img_resize)
+
+    # Export  
+    img_write(img, here::here(out$atlas, "cea_network.png"))
+    rm(img)
+    gc()
+  }
+  
+  # ----------------------------------------------------------------------------------------
   if ("species" %in% type) {
     # Species 
     out <- list()
     out$out <- here::here("figures","biotic")
     out$cnt <- here::here(out$out, "continuous")
     out$bin <- here::here(out$out, "binary")
+    out$cea_sp1 <- here::here("figures","cea_species","2010_2015")
+    out$cea_sp2 <- here::here("figures","cea_species","2016_2021")
+    out$cea_net1 <- here::here("figures","cea_network","2010_2015")
+    out$cea_net2 <- here::here("figures","cea_network","2016_2021")
     out$atlas <- here::here("figures", "atlas","biotic")
-    chk <- file.exists(out$cnt) & file.exists(out$bin)
+  
+    # Checks
+    chk <- file.exists(out$cnt) & 
+           file.exists(out$bin) &
+           file.exists(out$cea_sp1) &
+           file.exists(out$cea_sp2) &
+           file.exists(out$cea_net1) &
+           file.exists(out$cea_net2)
     stopifnot(chk)
+  
+    # Create and get files
     chk_create(out$atlas)
     cnt <- dir(out$cnt, full.names = TRUE)
     bin <- dir(out$bin, full.names = TRUE)
-    
+    sp1 <- dir(out$cea_sp1, full.names = TRUE)
+    sp2 <- dir(out$cea_sp2, full.names = TRUE)
+    net1 <- dir(out$cea_net1, full.names = TRUE)
+    net2 <- dir(out$cea_net2, full.names = TRUE)
+  
     # Names 
     nm <- basename(cnt) |>
           tools::file_path_sans_ext() |>
@@ -176,15 +229,23 @@ fig_atlas <- function(type = c("species","drivers")) {
           dplyr::rename(species = V1, aphiaID = V2) |>
           dplyr::mutate(species = gsub("_"," ",species)) |>
           dplyr::mutate(species = stringr::str_to_sentence(species))
-          
+  
     # Figures 
     for(i in 1:length(cnt)) {
       # Load images 
       i1 <- magick::image_read(cnt[i])
       i2 <- magick::image_read(bin[i])
+      i3 <- magick::image_read(sp1[i])
+      i4 <- magick::image_read(sp2[i])
+      i5 <- magick::image_read(net1[i])
+      i6 <- magick::image_read(net2[i])
+      
+      # Stack and combine images 
+      s1 <- magick::image_append(c(i1,i2))
+      s2 <- magick::image_append(c(i3,i4))
+      s3 <- magick::image_append(c(i5,i6))
+      img <- magick::image_append(c(s1,s2,s3), stack = TRUE)
 
-      # Combine images 
-      img <- magick::image_append(c(i1,i2))
 
       # Add border 
       ht <- magick::image_info(img)$height 
@@ -193,7 +254,14 @@ fig_atlas <- function(type = c("species","drivers")) {
       
       # Add text 
       img <- nm_title(img, nm$species[i])
-      img <- nm_sub(img, nm$aphiaID[i])
+      img <- nm_sub(img, nm$aphiaID[i])      
+      img <- nm_sub2(img, "Continuous distribution", t1_1)
+      img <- nm_sub2(img, "Binary distribution", t2_1)
+      img <- nm_sub2(img, "Species-scale cumulative effects - 2010-2015", t1_2)
+      img <- nm_sub2(img, "Species-scale cumulative effects - 2016-2021", t2_2)
+      img <- nm_sub2(img, "Network-scale cumulative effects - 2010-2015", t1_3)
+      img <- nm_sub2(img, "Network-scale cumulative effects - 2016-2021", t2_3)
+
       
       # Resize 
       img <- magick::image_resize(img, img_resize)
@@ -226,45 +294,37 @@ fig_atlas <- function(type = c("species","drivers")) {
           dplyr::mutate(path = here::here(out$out, glue::glue("{drivers}-{period}.png"))) |>
           dplyr::mutate(period2 = gsub("_"," - ",period)) |>
           dplyr::left_join(drList, by = "drivers")
-
+  
     # Figures 
     for(i in 1:nrow(drList)) {
       uid1 <- nm$drivers == drList$drivers[i] & nm$period == "2010_2015"
       uid2 <- nm$drivers == drList$drivers[i] & nm$period == "2016_2021"
-      
+  
       # Load images 
       i1 <- magick::image_read(nm$path[uid1])
       i2 <- magick::image_read(nm$path[uid2])
-
+  
       # Combine images 
       img <- magick::image_append(c(i1,i2))
-
+  
       # Add border 
       ht <- magick::image_info(img)$height 
       img <- magick::image_border(img, glue::glue("0x{hts}"), color = "#ffffff") |>
              magick::image_crop(glue::glue("0x{ht+hts}")) 
-      
+  
       # Add text       
       img <- nm_title(img, nm$group[uid1])
       img <- nm_sub(img, nm$description[uid1])
-      img <- nm_sub2(img, nm$period2[uid1], img1_loc)
-      img <- nm_sub2(img, nm$period2[uid2], img2_loc)
-      
+      img <- nm_sub2(img, nm$period2[uid1], t1_1)
+      img <- nm_sub2(img, nm$period2[uid2], t2_1)
+  
       # Resize 
       img <- magick::image_resize(img, img_resize)
-      
+  
       # Export  
       img_write(img, here::here(out$atlas, glue::glue("{nm$drivers[uid1]}.png")))
       rm(img)
       gc()
     }
-  
-  
-  
-  
-  
-  
   }
-
-
 }
---
+++ R/figures.R
@@ -0,0 +1,108 @@
+#' Export figures
+#'
+#' @export
+figures <- function() {
+  out <- list()
+  out$out <- here::here("figures")
+  out$biotic_cnt <- here::here(out$out,"biotic","continuous")
+  out$biotic_bin <- here::here(out$out,"biotic","binary")
+  out$abiotic <- here::here(out$out, "abiotic")
+  out$drivers <- here::here(out$out,"drivers")
+  out$cea_species <- here::here(out$out,"cea_species")
+  out$cea_network <- here::here(out$out,"cea_network")
+  out$cea <- here::here(out$out, "cea")
+  out$footprint <- here::here(out$out, "footprint")
+  out$exposure <- here::here(out$out, "exposure")
+  lapply(out, chk_create)
+  
+  # ---
+  plotDat <- function(dat, out, suffix = "") {
+    nm <- tools::file_path_sans_ext(names(dat))
+    png(
+      here::here(out, glue::glue("{nm}{suffix}.png")), 
+      res = param$figures$resolution, 
+      width = param$figures$width, 
+      height = param$figures$height, 
+      units = "mm", 
+      pointsize = param$figures$pointsize
+    )
+    plot_nceadfo(dat)
+    dev.off()
+  }
+  
+  # Species distribution continuous
+  dir(
+    c("data/data-biotic/marine_mammals/continuous",
+      "data/data-biotic/sea_birds/continuous", 
+      "data/data-biotic/marine_species/random_forest_regression_smoothing"), 
+    full.names = TRUE
+  ) |>
+  lapply(stars::read_stars) |>
+  lapply(plotDat, out$biotic_cnt)
+
+  # Species distribution binary
+  dir(
+    c("data/data-biotic/marine_mammals/binary",
+      "data/data-biotic/sea_birds/binary", 
+      "data/data-biotic/marine_species/random_forest_regression_binary"), 
+    full.names = TRUE
+  ) |>
+  lapply(stars::read_stars) |>
+  lapply(plotDat, out$biotic_bin)
+
+  # Abiotic  
+  dir("data/data-abiotic", full.names = TRUE, pattern = ".tif$") |>
+  lapply(stars::read_stars) |>
+  lapply(plotDat, out$abiotic)
+  
+  # Drivers 
+  dr <- here::here("data","cea_modules","drivers")
+  per <- dir(dr)
+  for(i in 1:length(per)) {
+    here::here(dr, per[i]) |>
+    dir(full.names = TRUE, pattern = ".tif$") |>
+    lapply(stars::read_stars) |>
+    lapply(plotDat, out$drivers)
+  }
+
+  # Footprint
+  dir(here::here("output","footprint"), full.names = TRUE) |>
+  lapply(stars::read_stars) |>
+  lapply(plotDat, out$footprint)
+
+  # Exposure
+  dir(here::here("output","exposure"), full.names = TRUE) |>
+  lapply(stars::read_stars) |>
+  lapply(plotDat, out$exposure)
+
+  # Species-scale cumulative effects assessment - species
+  dr <- here::here("output","cea_species")
+  per <- dir(dr)
+  outsp <- here::here(out$cea_species, per)
+  lapply(outsp, chk_create)
+  for(i in 1:length(per)) {
+    here::here(dr, per[i]) |>
+    dir(full.names = TRUE, pattern = ".tif$") |>
+    lapply(stars::read_stars) |>
+    lapply(plotDat, outsp[i])
+  }
+  
+  # Network-scale cumulative effects assessment - species
+  dr <- here::here("output","cea_network")
+  per <- dir(dr)
+  outsp <- here::here(out$cea_network, per)
+  lapply(outsp, chk_create)
+  for(i in 1:length(per)) {
+    here::here(dr, per[i], "cea") |>
+    dir(full.names = TRUE, pattern = ".tif$") |>
+    lapply(stars::read_stars) |>
+    lapply(plotDat, outsp[i])
+  }
+  
+  # Species-scale and network-scale cumulative effects assessment
+  dr <- here::here("output","cea")
+  dir(dr, full.names = TRUE) |>
+  lapply(stars::read_stars) |>
+  lapply(plotDat, out$cea)
+  
+}
