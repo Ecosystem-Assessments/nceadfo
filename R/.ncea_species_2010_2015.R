0efb63757b68f23a98067c4db5937a27bab8afa8
David Beauchesne <david.beauchesne@uqar.ca>
2024-01-22 09:31:13 -0500

    remove filter on data run

--- R/.ncea_species_2010_2015.R
+++ R/.ncea_species_2010_2015.R
@@ -1,18 +1,18 @@
-# install.packages(c(
-#   "devtools",
-#   "dplyr",
-#   "tidyr",
-#   "sf",
-#   "stars",
-#   "vroom",
-#   "here"
-# ))
-# devtools::install_github("ecosystem-assessments/motifcensus")
-# devtools::install_github("ecosystem-assessments/rcea")
-library(rcea)
-library(dplyr)
-library(tidyr)
-library(stars)
+# # install.packages(c(
+# #   "devtools",
+# #   "dplyr",
+# #   "tidyr",
+# #   "sf",
+# #   "stars",
+# #   "vroom",
+# #   "here"
+# # ))
+# # devtools::install_github("ecosystem-assessments/motifcensus")
+# # devtools::install_github("ecosystem-assessments/rcea")
+# library(rcea)
+# library(dplyr)
+# library(tidyr)
+# library(stars)
 
 # Specify and create output folder
 per <- "2010_2015"
@@ -35,8 +35,8 @@ drivers <- drivers[[per]]
 i <- as.numeric(commandArgs(trailingOnly = TRUE))
 rcea::ncea_species(
   focus = names(biotic)[i],
-  drivers[, 500:510, 300:310],
-  biotic[, 500:510, 300:310],
+  drivers, # [, 500:510, 300:310],
+  biotic, # [, 500:510, 300:310],
   species_sensitivity,
   metaweb,
   trophic_sensitivity,
