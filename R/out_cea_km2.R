76b40c5bb58d7f5a14a47bdffd0900bb4d13d4ee
David Beauchesne <david.beauchesne@uqar.ca>
2024-02-14 13:37:47 -0500

    modify script to export metrics per km2

--- R/out_cea_km2.R
+++ R/out_cea_km2.R
@@ -9,18 +9,25 @@ out_cea_km2 <- function() {
   per <- dir(here::here("output", "cea_species"))
   nper <- length(per)
 
-  # Species distributions
-  load(here::here("data", "FormatData", "bt.RData"))
-  hr <- data.frame(
-    species = colnames(bt),
-    area = colSums(bt, na.rm = TRUE)
-  )
-  rownames(hr) <- NULL
+  # Function to assess area of species and habitat distributions
+  area_units <- function(dat) {
+    size <- sf::st_area(dat)
+    size[[1]] <- set_units(size[[1]], km^2)
+    dat <- dat * size
+    df <- as.data.frame(dat) |>
+      dplyr::select(-x, -y) |>
+      colSums(na.rm = TRUE) |>
+      units::set_units(km^2)
+    data.frame(
+      name = names(df),
+      area_km2 = as.numeric(df),
+      row.names = NULL
+    )
+  }
 
-  # Species-scale cumulative effects assessment
-  for (i in seq_len(nper)) {
-    folder <- here::here("output", "cea_species", per[i])
-    files <- dir(folder, full.names = TRUE)
+  # Function to assess cea per km2
+  ceakm2 <- function(cea_input, area) {
+    files <- dir(cea_input, full.names = TRUE)
     nm <- basename(files) |>
       stringr::str_replace(".tif", "")
 
@@ -29,21 +36,55 @@ out_cea_km2 <- function() {
         split() |>
         as.data.frame() |>
         dplyr::select(-x, -y) |>
-        # colSums(na.rm = TRUE)
         colSums(na.rm = TRUE)
     }) |>
       dplyr::bind_rows() |>
-      dplyr::mutate(species = nm)
+      dplyr::mutate(name = nm)
 
     # Join together and divide by area
-    cekm <- dplyr::left_join(hr, dat, by = "species") |>
-      dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ .x / area)) |>
-      dplyr::select(-area)
-
-    # Export
-    write.csv(cekm, file = here::here(out, glue::glue("cea_km2_{per[i]}.csv")), row.names = FALSE)
+    cekm <- dplyr::left_join(area, dat, by = "name") |>
+      dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ .x / area_km2)) |>
+      dplyr::select(-area_km2)
   }
 
+  # Species and habitats area
+  load(here::here("data", "FormatData", "biotic.RData"))
+  load(here::here("data", "FormatData", "habitats.RData"))
+  biotic <- mask_aoi(biotic) |> area_units()
+  habitats <- mask_aoi(habitats) |> area_units()
+
+  # Effects per km^2
+  for (i in seq_len(nper)) {
+    # Species-scale assessment
+    ceakm2(
+      cea_input = here::here("output", "cea_species", per[i]),
+      area = biotic
+    ) |>
+      write.csv(
+        file = here::here(out, glue::glue("cea_km2_{per[i]}.csv")),
+        row.names = FALSE
+      )
+
+    # Habitat-scale assessment
+    ceakm2(
+      cea_input = here::here("output", "cea_habitats", per[i]),
+      area = habitats
+    ) |>
+      write.csv(
+        file = here::here(out, glue::glue("cea_habitats_km2_{per[i]}.csv")),
+        row.names = FALSE
+      )
+
+    # Indirect effects to species arising from habitats
+    ceakm2(
+      cea_input = here::here("output", "cea_indirect_habitats", per[i]),
+      area = biotic
+    ) |>
+      write.csv(
+        file = here::here(out, glue::glue("cea_indirect_habitats_km2_{per[i]}.csv")),
+        row.names = FALSE
+      )
+  }
 
   # NOTE: The network-scale assessment performed with `rcea::ncea` already provides these results.
   #       We only need to reformat and export them
@@ -61,7 +102,7 @@ out_cea_km2 <- function() {
         dplyr::mutate(
           dplyr::across(dplyr::where(is.numeric), ~ .x * 0.01)
         ) |>
-        dplyr::rename(species = vc)
+        dplyr::rename(name = vc)
     }
     net <- loaddat(net)
     direct <- loaddat(direct)
